3a2512591c5884061222a14fcb962b1494c7e998
Mike McTighe <m.mctighe10@gmail.com>
2019-09-17 00:40:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,10 +4,7 @@ end
 
 fido = Dog.new
 fido.name = "Fido"
-fido.name 
-
-
-jay_z = Person.new
-jay_z.name = "Shawn Carter"
-jay_z.name
-  => "Shawn Carter"
+fido.name
+pug = Dog.new
+pug.name = "Pug"
+pug.name
