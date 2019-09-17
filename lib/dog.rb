2a00c9260313ec693ca2f9d26e9fcd279dbb660d
Mike McTighe <m.mctighe10@gmail.com>
2019-09-17 00:38:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,12 @@ class Dog
   attr_accessor :name
 end
 
-fido = Dog.new("Fido")
-fido.name
-pug = Dog.new("Pug")
-pug.name
+fido = Dog.new
+fido.name = "Fido"
+fido.name 
+
+
+jay_z = Person.new
+jay_z.name = "Shawn Carter"
+jay_z.name
+  => "Shawn Carter"
